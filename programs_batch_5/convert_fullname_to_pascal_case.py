b952fdcff09dcaea5c7bf86d6e11f0a2e5ef9f17
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-19 12:55:24 +0800

    Added a prompt that tells user to input their name in incorrect spacing.

---
+++ programs_batch_5/convert_fullname_to_pascal_case.py
@@ -0,0 +1,5 @@
+# Prompt the user to enter their full name with incorrect casing.  
+# Convert the input to Pascal Case and print.  
+
+fullname = input("Enter your full name in incorrect casing: ")
+print(fullname)

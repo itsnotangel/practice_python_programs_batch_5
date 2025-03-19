14701f2dd84771839050e12d998727f7c939886b
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-19 11:56:51 +0800

    Fixed spacing.

--- programs_batch_5/remove_leading_spaces.py
+++ programs_batch_5/remove_leading_spaces.py
@@ -1,6 +1,6 @@
 # Prompt the user to enter their full name with leading spaces.  
 # Remove the leading spaces and print the cleaned name.  
 
-fullname = input("Enter your fullname: ")
+fullname = input("Enter your full name: ")
 formatted_name = fullname.lstrip()
-print (formatted_name)
+print(formatted_name)

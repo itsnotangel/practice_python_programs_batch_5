6da2f16d3947b6a51a57a1e3e01c9e4bb688acee
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-19 12:48:54 +0800

    Added a prompt that ask users to input their full name.

---
+++ programs_batch_5/count_characters_fullname.py
@@ -0,0 +1,5 @@
+# Prompt the user to enter their full name.  
+# Count the number of characters in the input, including spaces, and print.
+
+fullname = input("Enter your full name: ")
+print(fullname)

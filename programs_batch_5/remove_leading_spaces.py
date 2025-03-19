4056ded3622f8286a1462285cdc08820a1605f57
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-19 11:49:15 +0800

    Added a prompt that tell users to input their full name.

---
+++ programs_batch_5/remove_leading_spaces.py
@@ -0,0 +1,5 @@
+# Prompt the user to enter their full name with leading spaces.  
+# Remove the leading spaces and print the cleaned name.  
+
+fullname = input("Enter your fullname: ")
+print (fullname)

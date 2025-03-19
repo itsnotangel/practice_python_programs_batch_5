b9463bfe782155b48205180a6a24ff897e099635
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-19 13:01:52 +0800

    Added a prompt that tells user to input their full name in incorrect casing.

---
+++ programs_batch_5/convert_fullname_to_snake_case.py
@@ -0,0 +1,6 @@
+# Prompt the user to enter their full name with incorrect casing.  
+# Convert the input to snake_case and print.
+
+fullname = input("Enter your full name in incorrect casing: ")
+print(fullname)
+

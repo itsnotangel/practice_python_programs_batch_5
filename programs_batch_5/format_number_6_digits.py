c3aca4798c1b67e3d28e4dfdfc6ee77f976a8355
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-19 12:00:41 +0800

    Added a prompt that tells user to input number.

---
+++ programs_batch_5/format_number_6_digits.py
@@ -0,0 +1,6 @@
+# Prompt the user to enter a number between 0 and 1000.
+# Format the number to be displayed (6-digit value).
+# Add leading zeros to ensure the number has exactly 6 digits.
+
+number = int(input("Enter a number (0-1000): "))
+print(number)
